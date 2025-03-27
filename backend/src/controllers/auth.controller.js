4f289498935f4a887d92a9aa78d21c004fc4017b
Harun Selçuk Çetin <1910205014@ogrenci.karabuk.edu.tr>
2025-03-27 16:38:26 +0300

    auth middleware
    auth middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -73,10 +73,12 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
     try {
-        res.cookie("jwt","",{maxAge:0});
-        res.status(200).json({message:"Logout Successfully"});
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logout Successfully" });
     } catch (error) {
         console.log("Auth Controller -> Logout Error: ", error.message);
         res.status(500).json({ message: "Internal Server Error" })
     }
 }
+
+export const updateProfile = (req, res) => { }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from 'dotenv';
-
+import cookieParser from 'cookie-parser';
 import { connectDB } from "./lib/db.js";
 //routers
 import authRoutes from "./routes/auth.route.js";
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 
 
